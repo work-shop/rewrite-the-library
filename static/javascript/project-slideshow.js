44d05bddf861bd287e0f5823e7c472dfc3c15263
Greg Nemes <mail@gregnemes.com>
2015-12-10 14:02:45 -0500

    have a reasonably well working slideshow

--- static/javascript/project-slideshow.js
+++ static/javascript/project-slideshow.js
@@ -1,7 +1,33 @@
 	
+//elements
+var container = $('.project-slideshow');
+var	track = $('.project-slideshow-track');
+var	slides = $('.project-slide');	
+
+//total number of slides
+nSlides = slides.length;
+
+
+//scale factor for slides
+var slideScale = 1.2;
+
+//position of the track along it's axis
+var trackPosition = 0;
+
+
 jQuery(document).ready(function($) {
 
 	slideshowSetup();
+
+	$('.slideshow-previous').click(function(event) {
+	  	event.preventDefault();
+	  	slideshowIncrement('previous');
+	});	
+
+	$('.slideshow-next').click(function(event) {
+	  	event.preventDefault();
+	  	slideshowIncrement('next');
+	});
 
 });
 
@@ -10,32 +36,53 @@ $(window).on('resize', slideshowSetup);
 
 function slideshowSetup(){
 
-	//window proportions
+	//width of containing element
 	w = $('.hero-project').width();
-	h = $(window).height();
-
-	//elements
-	container = $('.project-slideshow');
-	track = $('.project-slideshow-track');
-	slides = $('.project-slide');
-
-	//number of slides
-	nSlides = slides.length;
-
-	//scale factor for slides
-	slideScale = 1.2;
-	
-	//width of track: number of slides(scaled) * window width
-	trackWidth = nSlides * (w * slideScale);
 
 	//width of slide: window width * slide scale factor
 	slideWidth = w * slideScale;
-	 
-	//overall sizing
-	container.width(trackWidth);
-	track.width(trackWidth);
-	slides.width(slideWidth);
 
+	//width of track: number of slides(scaled) * window width
+	trackWidth = nSlides * (w * slideScale);	
+
+	//overall sizing
+	slides.width(slideWidth);
+	track.width(trackWidth);
+
+}
+
+
+function slideshowIncrement(direction){
+
+	//width of containing element
+	w = $('.hero-project').width();
+
+	//width of track
+	trackWidth = nSlides * (w * slideScale);
+	 
+	//distance of one slide translation
+	trackTranslation = -1 * ( (w / trackWidth) * 100);
+
+	//maximum track translation before stopping
+	trackMax = (trackTranslation * (nSlides-1));
+	console.log(trackMax);
+
+	if(direction == 'next'){
+		//add the translation distance to the current track position
+		trackPosition += trackTranslation;	
+		if(trackPosition < trackMax){
+			trackPosition = trackMax;
+		}		
+	}
+	else{
+		//subtract the translation distance from the current track position
+		trackPosition -= trackTranslation;			
+		if(trackPosition > 0){
+			trackPosition = 0;
+		}
+	}
+
+	track.css('transform','translate(' + trackPosition + '%)');
 
 }
 
